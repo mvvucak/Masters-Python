6283457df64dbf6660239e1e8ce35e1930330777
Matej Vucak <wepster34@hotmail.com>
2018-08-01 12:22:16 +0100

    Complete Conversion

--- ImportGNPS.py
+++ ImportGNPS.py
@@ -3,7 +3,8 @@ import matplotlib as plt
 from itertools import islice
 import os
 
-datapath = "E:\\Development Project\\Data\\GNPS"
+datapath = "E:\\Development Project\\Data\\GNPS Python Test"
+binned_datapath = "E:\\Development Project\\Data\\GNPS Python Test Binned"
 testpath = "E:\\Development Project\\Data\\GNPS\\CCMSLIB00000004552.ms"
 
 
@@ -18,12 +19,35 @@ def find_max_mass():
                 if ' ' in line:
                     next_mass = int(round(float(line.split()[0])))
                     mass_list.append(next_mass)
-                 # print(max_mass)
-                 # print(line.split()[0])
-                 # print(int(round(float(line.split()[0]))))
-                 # print(line.split()[1])
-
+                    # print(line.split()[0])
+                    # print(int(round(float(line.split()[0]))))
+                    # print(line.split()[1])
     return max(mass_list)
 
-max_mass = find_max_mass()
-print(max_mass)
+def write_binned_files():
+    max_bins = find_max_mass() + 1
+    for file in os.listdir(datapath):
+        filepath = os.path.join(datapath, file)
+        binned_values = np.zeros(max_bins)
+        with open(filepath, 'r') as f:
+            filename = f.name
+
+            unsplit_lines = list(islice(f, 9, None))
+            for line in unsplit_lines:
+                if ' ' in line:  # Only lines with mass and intensity values have a space. Ignores label/blank lines
+                    split_line = line.split()
+                    mass = int(round(float(split_line[0])))  # Serves as index for binned values array
+                    intensity = float(split_line[1])
+                    #print ("The intensity for mass " + str(mass) + " is " + str(binned_values[mass]))
+                    binned_values[mass] = binned_values[mass] + intensity
+                    #print("The intensity for mass " + str(mass) + " is " + str(binned_values[mass]))
+        binned_filename = file.split(".")[0] + " Binned.txt"
+        binned_filepath = os.path.join(binned_datapath, binned_filename)
+        with open(binned_filepath, 'w') as f:
+            for intensity in binned_values:
+                f.write(str(intensity) + "\n")
+
+
+
+
+write_binned_files()
